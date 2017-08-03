98460a4d60db9e5ec684f47106c8063ba91f9c7e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-08-03 09:39:33 -0300

    misc

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,35 @@
 from conans import ConanFile, CMake
+import os
 
-
-class BitprimnetworkConan(ConanFile):
+class BitprimNetworkConan(ConanFile):
     name = "bitprim-network"
     version = "0.1"
     license = "http://www.boost.org/users/license.html"
-    url = "https://github.com/bitprim/bitprim-database"
-    description = "Description of Bitprimnetwork"
+    url = "https://github.com/bitprim/bitprim-network"
+    description = "Bitcoin P2P Network Library"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
-    exports_sources = "src/*"
+    exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-networkConfig.cmake.in", "include/*"
     package_files = "build/lbitprim-network.a"
+    build_policy = "missing"
 
-#    def build(self):
-#        cmake = CMake(self)
-#        self.run('cmake %s/src %s' % (self.source_folder, cmake.command_line))
-#        self.run("cmake --build . %s" % cmake.build_config)
+    requires = (("bitprim-conan-boost/1.64.0@bitprim/stable"),
+                ("bitprim-core/0.1@bitprim/stable"))
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_dir=self.conanfile_directory)
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.h", "", "include")
 
     def package(self):
-        self.copy("*.h", dst="include", src="src")
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*.hpp", dst="include", src="include")
+        self.copy("*.ipp", dst="include", src="include")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
@@ -28,4 +37,5 @@ class BitprimnetworkConan(ConanFile):
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
+        self.cpp_info.includedirs = ['include']
         self.cpp_info.libs = ["bitprim-network"]
