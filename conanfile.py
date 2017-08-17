242b9c59f78fb82109175450ea4ea91c5bf1422a
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-17 13:27:16 -0300

    conan improvements

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,7 @@
 from conans import ConanFile, CMake
-import os
+
+def option_on_off(option):
+    return "ON" if option else "OFF"
 
 class BitprimNetworkConan(ConanFile):
     name = "bitprim-network"
@@ -8,8 +10,28 @@ class BitprimNetworkConan(ConanFile):
     url = "https://github.com/bitprim/bitprim-network"
     description = "Bitcoin P2P Network Library"
     settings = "os", "compiler", "build_type", "arch"
+    
     options = {"shared": [True, False]}
     default_options = "shared=False"
+
+
+    options = {"shared": [True, False],
+               "fPIC": [True, False],
+               "with_tests": [True, False],
+               "with_litecoin": [True, False],
+    }
+
+    default_options = "shared=False", \
+        "fPIC=True", \
+        "with_tests=True", \
+        "with_litecoin=False"
+
+# option(USE_CONAN "Use Conan Build Tool." OFF)
+# option(ENABLE_SHARED "" OFF)
+# option(WITH_TESTS "Compile with unit tests." ON)
+# option(WITH_LITECOIN "Compile with Litecoin support." OFF)
+
+
     generators = "cmake"
     exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-networkConfig.cmake.in", "include/*", "test/*"
     package_files = "build/lbitprim-network.a"
@@ -20,6 +42,14 @@ class BitprimNetworkConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+
+        cmake.definitions["USE_CONAN"] = "ON"
+        cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = "ON"
+        cmake.definitions["ENABLE_SHARED"] = option_on_off(self.options.shared)
+        cmake.definitions["ENABLE_POSITION_INDEPENDENT_CODE"] = option_on_off(self.options.fPIC)
+        cmake.definitions["WITH_TESTS"] = option_on_off(self.options.with_tests)
+        cmake.definitions["WITH_LITECOIN"] = option_on_off(self.options.with_litecoin)
+
         cmake.configure(source_dir=self.conanfile_directory)
         cmake.build()
 
