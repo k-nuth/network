7b2e375b1fbc97a68f25a36ac80411025e90f6a8
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-19 15:33:25 -0300

    MD build

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
                 and not options["bitprim-network:shared"] \
-                and (not "compiler.runtime" in settings or not settings["compiler.runtime"] == "MD"):
+                and (not "compiler.runtime" in settings or not settings["compiler.runtime"] == "MT"):
              filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
