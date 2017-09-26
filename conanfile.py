1fa5f949b3eb5416d7e05aa7fc85d09c08b61a7a
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-09-26 10:27:13 -0300

    change conan channel

--- conanfile.py
+++ conanfile.py
@@ -52,7 +52,7 @@ class BitprimNetworkConan(ConanFile):
     build_policy = "missing"
 
     requires = (("bitprim-conan-boost/1.64.0@bitprim/stable"),
-                ("bitprim-core/0.2@bitprim/testing"))
+                ("bitprim-core/0.2@bitprim/stable"))
 
     def build(self):
         cmake = CMake(self)
