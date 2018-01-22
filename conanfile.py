7038c99cd06cf22da770471a892eb13d3157153f
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-01-22 12:32:52 -0300

    dev 0.7 version

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ def option_on_off(option):
 
 class BitprimNetworkConan(ConanFile):
     name = "bitprim-network"
-    version = "0.6"
+    version = "0.7"
     license = "http://www.boost.org/users/license.html"
     url = "https://github.com/bitprim/bitprim-network"
     description = "Bitcoin P2P Network Library"
@@ -57,7 +57,7 @@ class BitprimNetworkConan(ConanFile):
     build_policy = "missing"
 
     requires = (("bitprim-conan-boost/1.64.0@bitprim/stable"),
-                ("bitprim-core/0.6@bitprim/testing"))
+                ("bitprim-core/0.7@bitprim/testing"))
 
     def build(self):
         cmake = CMake(self)
