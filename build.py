8c5cb3e12cb86c0127c563c9d83fc7fb7c9eef99
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-01-24 16:40:51 -0300

    build MT and MD for MSVC

--- build.py
+++ build.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
-                and not options["bitprim-network:shared"] \
-                and (not "compiler.runtime" in settings or not settings["compiler.runtime"] == "MT"):
+                and not options["bitprim-network:shared"]:
 
             env_vars["BITPRIM_BUILD_NUMBER"] = os.getenv('BITPRIM_BUILD_NUMBER', '-')
                 
