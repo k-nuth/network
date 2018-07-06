5bcb0e70700e6d630ba0379fdf250972c14bd6a4
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-07-06 12:36:41 -0300

    CI fixes (#16)

--- conanfile.py
+++ conanfile.py
@@ -18,22 +18,20 @@
 #
 
 import os
-# import sys
-from conans import ConanFile, CMake
-from conans import __version__ as conan_version
-from conans.model.version import Version
+from conans import CMake
 from ci_utils import option_on_off, get_version, get_conan_req_version, march_conan_manip, pass_march_to_compiler
+from ci_utils import BitprimConanFile
 
-class BitprimNetworkConan(ConanFile):
+class BitprimNetworkConan(BitprimConanFile):
     name = "bitprim-network"
-    version = get_version()
+    # version = get_version()
     license = "http://www.boost.org/users/license.html"
     url = "https://github.com/bitprim/bitprim-network"
     description = "Bitcoin P2P Network Library"
     settings = "os", "compiler", "build_type", "arch"
 
-    if Version(conan_version) < Version(get_conan_req_version()):
-        raise Exception ("Conan version should be greater or equal than %s. Detected: %s." % (get_conan_req_version(), conan_version))
+    # if Version(conan_version) < Version(get_conan_req_version()):
+    #     raise Exception ("Conan version should be greater or equal than %s. Detected: %s." % (get_conan_req_version(), conan_version))
 
     options = {"shared": [True, False],
                "fPIC": [True, False],
@@ -52,35 +50,16 @@ class BitprimNetworkConan(ConanFile):
         "fix_march=False", \
         "verbose=True"
 
-
     generators = "cmake"
     exports = "conan_*", "ci_utils/*"
     exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-networkConfig.cmake.in", "bitprimbuildinfo.cmake", "include/*", "test/*"
     package_files = "build/lbitprim-network.a"
     build_policy = "missing"
 
-    @property
-    def msvc_mt_build(self):
-        return "MT" in str(self.settings.compiler.runtime)
-
-    @property
-    def fPIC_enabled(self):
-        if self.settings.compiler == "Visual Studio":
-            return False
-        else:
-            return self.options.fPIC
-
-    @property
-    def is_shared(self):
-        if self.options.shared and self.msvc_mt_build:
-            return False
-        else:
-            return self.options.shared
 
     def requirements(self):
         self.requires("boost/1.66.0@bitprim/stable")
         self.requires("bitprim-core/0.X@%s/%s" % (self.user, self.channel))
-
 
     def config_options(self):
         if self.settings.arch != "x86_64":
