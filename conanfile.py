5ae841fb0222c39002877d8537c6212d6e87b4c4
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-01 17:53:36 -0300

    compiler flags

--- conanfile.py
+++ conanfile.py
@@ -41,14 +41,8 @@ class BitprimNetworkConan(BitprimConanFile):
                "fix_march": [True, False],
                "verbose": [True, False],
                "use_domain": [True, False],
-               "db_transaction_unconfirmed": [True, False],
-               "db_spends": [True, False],
-               "db_history": [True, False],
-               "db_stealth": [True, False],
-               "db_unspent_libbitcoin": [True, False],
-               "db_legacy": [True, False],
-               "db_new": [True, False],
-
+               "cxxflags": "ANY",
+               "cflags": "ANY",
     }
 
     default_options = "shared=False", \
@@ -59,13 +53,8 @@ class BitprimNetworkConan(BitprimConanFile):
         "fix_march=False", \
         "verbose=False", \
         "use_domain=False", \
-        "db_transaction_unconfirmed=True", \
-        "db_spends=True", \
-        "db_history=True", \
-        "db_stealth=True", \
-        "db_unspent_libbitcoin=True", \
-        "db_legacy=True", \
-        "db_new=False"
+        "cxxflags=_DUMMY_", \
+        "cflags=_DUMMY_"
 
     generators = "cmake"
     exports = "conan_*", "ci_utils/*"
@@ -104,16 +93,7 @@ class BitprimNetworkConan(BitprimConanFile):
             march_conan_manip(self)
             self.options["*"].microarchitecture = self.options.microarchitecture
 
-        self.options["*"].db_transaction_unconfirmed = self.options.db_transaction_unconfirmed
-        self.options["*"].db_spends = self.options.db_spends
-        self.options["*"].db_history = self.options.db_history
-        self.options["*"].db_stealth = self.options.db_stealth
-        self.options["*"].db_unspent_libbitcoin = self.options.db_unspent_libbitcoin
-        self.options["*"].db_legacy = self.options.db_legacy
-        self.options["*"].db_new = self.options.db_new
-
         self.options["*"].use_domain = self.options.use_domain
-
 
         self.options["*"].currency = self.options.currency
         self.output.info("Compiling for currency: %s" % (self.options.currency,))
@@ -122,6 +102,8 @@ class BitprimNetworkConan(BitprimConanFile):
         self.info.options.with_tests = "ANY"
         self.info.options.verbose = "ANY"
         self.info.options.fix_march = "ANY"
+        self.info.options.cxxflags = "ANY"
+        self.info.options.cflags = "ANY"
 
         #For Bitprim Packages libstdc++ and libstdc++11 are the same
         if self.settings.compiler == "gcc" or self.settings.compiler == "clang":
@@ -140,20 +122,18 @@ class BitprimNetworkConan(BitprimConanFile):
         cmake.definitions["CURRENCY"] = self.options.currency
         cmake.definitions["USE_DOMAIN"] = option_on_off(self.options.use_domain)
 
-        cmake.definitions["DB_TRANSACTION_UNCONFIRMED"] = option_on_off(self.options.db_transaction_unconfirmed)
-        cmake.definitions["DB_SPENDS"] = option_on_off(self.options.db_spends)
-        cmake.definitions["DB_HISTORY"] = option_on_off(self.options.db_history)
-        cmake.definitions["DB_STEALTH"] = option_on_off(self.options.db_stealth)
-        cmake.definitions["DB_UNSPENT_LIBBITCOIN"] = option_on_off(self.options.db_unspent_libbitcoin)
-        cmake.definitions["DB_LEGACY"] = option_on_off(self.options.db_legacy)
-        cmake.definitions["DB_NEW"] = option_on_off(self.options.db_new)
-
         if self.settings.compiler != "Visual Studio":
             # cmake.definitions["CONAN_CXX_FLAGS"] += " -Wno-deprecated-declarations"
             cmake.definitions["CONAN_CXX_FLAGS"] = cmake.definitions.get("CONAN_CXX_FLAGS", "") + " -Wno-deprecated-declarations"
 
         if self.settings.compiler == "Visual Studio":
             cmake.definitions["CONAN_CXX_FLAGS"] = cmake.definitions.get("CONAN_CXX_FLAGS", "") + " /DBOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE"
+
+        if self.options.cxxflags != "_DUMMY_":
+            cmake.definitions["CONAN_CXX_FLAGS"] = cmake.definitions.get("CONAN_CXX_FLAGS", "") + " " + str(self.options.cxxflags)
+        if self.options.cflags != "_DUMMY_":
+            cmake.definitions["CONAN_C_FLAGS"] = cmake.definitions.get("CONAN_C_FLAGS", "") + " " + str(self.options.cflags)
+
 
         cmake.definitions["MICROARCHITECTURE"] = self.options.microarchitecture
         cmake.definitions["BITPRIM_PROJECT_VERSION"] = self.version
