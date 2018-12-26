162400ff64551440179de4827b2d94f825eef49d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-12-26 15:43:49 -0300

    GCC C++11 ABI fixer

--- conanfile.py
+++ conanfile.py
@@ -86,6 +86,8 @@ class BitprimNetworkConan(BitprimConanFile):
                 self.options.remove("shared")
 
     def configure(self):
+        BitprimConanFile.configure(self)
+
         if self.settings.arch == "x86_64" and self.options.microarchitecture == "_DUMMY_":
             del self.options.fix_march
             # self.options.remove("fix_march")
@@ -101,16 +103,18 @@ class BitprimNetworkConan(BitprimConanFile):
         self.output.info("Compiling for currency: %s" % (self.options.currency,))
 
     def package_id(self):
+        BitprimConanFile.package_id(self)
+
         self.info.options.with_tests = "ANY"
         self.info.options.verbose = "ANY"
         self.info.options.fix_march = "ANY"
         self.info.options.cxxflags = "ANY"
         self.info.options.cflags = "ANY"
 
-        #For Bitprim Packages libstdc++ and libstdc++11 are the same
-        if self.settings.compiler == "gcc" or self.settings.compiler == "clang":
-            if str(self.settings.compiler.libcxx) == "libstdc++" or str(self.settings.compiler.libcxx) == "libstdc++11":
-                self.info.settings.compiler.libcxx = "ANY"
+        # #For Bitprim Packages libstdc++ and libstdc++11 are the same
+        # if self.settings.compiler == "gcc" or self.settings.compiler == "clang":
+        #     if str(self.settings.compiler.libcxx) == "libstdc++" or str(self.settings.compiler.libcxx) == "libstdc++11":
+        #         self.info.settings.compiler.libcxx = "ANY"
 
     def build(self):
         cmake = CMake(self)
