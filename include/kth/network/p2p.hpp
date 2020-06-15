f2367b8343b8acf54696707d60d036fb422e9dfb
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-15 20:51:00 -0300

    improvements in p2p

--- include/kth/network/p2p.hpp
+++ include/kth/network/p2p.hpp
@@ -12,7 +12,9 @@
 #include <memory>
 #include <string>
 #include <vector>
+
 #include <kth/domain.hpp>
+
 #include <kth/network/channel.hpp>
 #include <kth/network/define.hpp>
 #include <kth/network/hosts.hpp>
