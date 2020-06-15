5a61586a33ec57a8ac869a896387bd35965be7aa
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-15 19:15:00 -0300

    code style improvements for acceptor

--- include/kth/network/acceptor.hpp
+++ include/kth/network/acceptor.hpp
@@ -14,8 +14,7 @@
 #include <kth/network/define.hpp>
 #include <kth/network/settings.hpp>
 
-namespace kth {
-namespace network {
+namespace kth::network {
 
 /// Create inbound socket connections.
 /// This class is thread safe against stop.
