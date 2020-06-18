708d0495e39b0bd6fe0abd78b6c093ca1015b28c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-18 19:24:00 -0300

    code style in session_outbound needed to be improved

--- src/sessions/session_outbound.cpp
+++ src/sessions/session_outbound.cpp
@@ -23,8 +23,7 @@ using namespace std::placeholders;
 
 session_outbound::session_outbound(p2p& network, bool notify_on_connect)
     : session_batch(network, notify_on_connect)
-    , CONSTRUCT_TRACK(session_outbound)
-{}
+    , CONSTRUCT_TRACK(session_outbound) {}
 
 // Start sequence.
 // ----------------------------------------------------------------------------
