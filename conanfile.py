4e168de89356fc7941c5553adb4648649d7efed7
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-05-31 09:50:07 -0300

    update dependencies

--- conanfile.py
+++ conanfile.py
@@ -64,7 +64,7 @@ class KnuthNetworkConan(KnuthConanFile):
         self.requires("domain/0.X@%s/%s" % (self.user, self.channel))
 
         if self.options.tests:
-            self.requires("catch2/2.13.4")
+            self.requires("catch2/2.13.6")
 
     def config_options(self):
         KnuthConanFile.config_options(self)
