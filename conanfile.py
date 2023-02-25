8cdafba29ccf558fceb1f7a407930372760fe2f3
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-02-26 00:28:21 +0100

    misc

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import os
-from conans import CMake
+from conan import CMake
 from kthbuild import option_on_off, march_conan_manip, pass_march_to_compiler
 from kthbuild import KnuthConanFile
 
@@ -54,21 +54,23 @@ class KnuthNetworkConan(KnuthConanFile):
         "log": "spdlog",
     }
 
-    generators = "cmake"
+    # generators = "cmake"
     exports = "conan_*", "ci_utils/*"
     exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "kth-networkConfig.cmake.in", "knuthbuildinfo.cmake", "include/*", "test/*"
     package_files = "build/lkth-network.a"
-    build_policy = "missing"
+    # build_policy = "missing"
 
+    def build_requirements(self):
+        if self.options.tests:
+            self.test_requires("catch2/3.3.1")
 
     def requirements(self):
         self.requires("domain/0.X@%s/%s" % (self.user, self.channel))
 
-        if self.options.tests:
-            self.requires("catch2/3.3.1")
-
     def validate(self):
         KnuthConanFile.validate(self)
+        if self.info.settings.compiler.cppstd:
+            check_min_cppstd(self, "20")
 
     def config_options(self):
         KnuthConanFile.config_options(self)
@@ -80,9 +82,18 @@ class KnuthNetworkConan(KnuthConanFile):
         self.options["*"].log = self.options.log
         self.output.info("Compiling with log: %s" % (self.options.log,))
 
-
     def package_id(self):
         KnuthConanFile.package_id(self)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        # tc.variables["CMAKE_VERBOSE_MAKEFILE"] = True
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
         cmake = self.cmake_basis()
@@ -91,7 +102,8 @@ class KnuthNetworkConan(KnuthConanFile):
         cmake.definitions["LOG_LIBRARY"] = self.options.log
         cmake.definitions["CONAN_DISABLE_CHECK_COMPILER"] = option_on_off(True)
 
-        cmake.configure(source_dir=self.source_folder)
+        # cmake.configure(source_dir=self.source_folder)
+        cmake.configure()
         if not self.options.cmake_export_compile_commands:
             cmake.build()
             if self.options.tests:
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,6 @@
-// Copyright (c) 2016-2023 Knuth Project developers.
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// // Copyright (c) 2016-2023 Knuth Project developers.
+// // Distributed under the MIT software license, see the accompanying
+// // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+// #define CATCH_CONFIG_MAIN
+// #include <catch2/catch_all.hpp>
