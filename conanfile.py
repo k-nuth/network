6b67735e67022a1ad53e03fa69ab8a86788fa5fb
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-05-21 18:10:12 +0200

    upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -53,7 +53,7 @@ class KnuthNetworkConan(KnuthConanFileV2):
             self.test_requires("catch2/3.6.0")
 
     def requirements(self):
-        self.requires("domain/0.34.0", transitive_headers=True, transitive_libs=True)
+        self.requires("domain/0.35.0", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
         KnuthConanFileV2.validate(self)
