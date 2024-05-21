2819770c3fd762f31493977fb030c746fb1cfe93
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-05-21 14:04:57 +0200

    feat: bump dependencies

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,7 @@ class KnuthNetworkConan(KnuthConanFileV2):
 
     def build_requirements(self):
         if self.options.tests:
-            self.test_requires("catch2/3.5.2")
+            self.test_requires("catch2/3.6.0")
 
     def requirements(self):
         self.requires("domain/0.34.0", transitive_headers=True, transitive_libs=True)
