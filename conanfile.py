a92dfeb9b3fc93671cfae376b8b741f5fe9f0c83
Fernando Pelliccioni <fpelliccioni@gmail.com>
2025-05-01 13:13:23 +0200

    release v0.52.0

--- conanfile.py
+++ conanfile.py
@@ -54,7 +54,7 @@ class KnuthNetworkConan(KnuthConanFileV2):
             self.test_requires("catch2/3.6.0")
 
     def requirements(self):
-        self.requires("domain/0.43.0", transitive_headers=True, transitive_libs=True)
+        self.requires("domain/0.44.0", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
         KnuthConanFileV2.validate(self)
